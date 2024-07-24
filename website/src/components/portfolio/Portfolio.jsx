f685af00246cc23bc5cb2eee5159a5c2f9f2fb78
martinsjoeborg <113284827+martinsjoeborg@users.noreply.github.com>
2024-07-24 21:29:30 +0200

    ll

--- website/src/components/portfolio/Portfolio.jsx
+++ website/src/components/portfolio/Portfolio.jsx
@@ -1,4 +1,5 @@
 import "./Portfolio.css"
+import wealthroneLogo from "../../assets/img/wealthrone-logo.png"
 
 const Portfolio = () => {
     return (
@@ -6,12 +7,14 @@ const Portfolio = () => {
             <h2>Portfolio</h2>
             <div className="portfolio-content">
                 <div className="the-things">
+                    <div className="wealthrone thing">
+                        <img src={wealthroneLogo} alt="" className="wealthronelogo"/>
+                    </div>
                     <div className="crypto-explorer thing"></div>
                     <div className="restaurant thing"></div>
                     <div className="nodeChain thing"></div>
-                    <div className="crypto-explorer thing"></div>
-                    <div className="restaurant thing"></div>
-                    <div className="nodeChain thing"></div>
+                    <div className="thing5 thing"></div>
+                    <div className="thing6 thing"></div>
                     
 
                 </div>
