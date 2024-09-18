e1baa0131efbc463294d9566c39ca6812e54dd6b
martinsjoeborg <113284827+martinsjoeborg@users.noreply.github.com>
2024-09-18 14:19:11 +0200

    contact update

--- website/src/components/contact/Contact.jsx
+++ website/src/components/contact/Contact.jsx
@@ -1,53 +1,58 @@
 import "./Contact.css"
-import { useState } from "react";
-import emailjs from '@emailjs/browser';
+import React from "react";
+import Swal from 'sweetalert2';
 
 const Contact = () => {
-    const [isSubmitting, setIsSubmitting] = useState(false);
-    const [stateMessage, setStateMessage] = useState(null);
 
-    const sendEmail = (e) => {
-      e.persist();
-      e.preventDefault();
-      setIsSubmitting(true);
-      emailjs
-        .sendForm(
-          process.env.REACT_APP_SERVICE_ID,
-          process.env.REACT_APP_TEMPLATE_ID,
-          e.target,
-          process.env.REACT_APP_PUBLIC_KEY
-        )
-        .then(
-          (result) => {
-            setStateMessage('Message sent!');
-            setIsSubmitting(false);
-            setTimeout(() => {
-              setStateMessage(null);
-            }, 5000); // hide message after 5 seconds
-          },
-          (error) => {
-            setStateMessage('Something went wrong, please try again later');
-            setIsSubmitting(false);
-            setTimeout(() => {
-              setStateMessage(null);
-            }, 5000); // hide message after 5 seconds
-          }
-        );
-      
-      // Clears the form after sending the email
-      e.target.reset();
-    };
-    return (
-      <form onSubmit={sendEmail} className="form">
-        <label>Name</label>
-        <input type="text" name="user_name" />
-        <label>Email</label>
-        <input type="email" name="user_email" />
-        <label>Message</label>
-        <textarea name="message" />
-        <input type="submit" value="Send" disabled={isSubmitting} />
-        {stateMessage && <p>{stateMessage}</p>}
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "5a3db5ad-8bdd-4f23-b2fe-5acdfdf7a67c");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      Swal.fire({
+        title: "Thank you!",
+        text: "I will get back to you shortly.",
+        icon: "Close"
+      });
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
+  return (
+    <section className="contact">
+      <h2>Contact Me</h2>
+      <form onSubmit={onSubmit}>
+        
+        <div className="input-box">
+          <label>Full Name</label>
+          <input type="text" className="field" placeholder="Enter your name" name="name" required />
+        </div>
+        <div className="input-box">
+          <label>Email Address</label>
+          <input type="email" className="field" placeholder="Enter your email" name="email" required />
+        </div>
+        <div className="input-box">
+          <label>Your Message</label>
+          <textarea name="message" className="field mess" placeholder="Enter your message" required></textarea>
+        </div>
+        <button type="submit">Send</button>
       </form>
-    );
+    </section>
+  )
   };
   export default Contact;
